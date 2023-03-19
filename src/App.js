a92507bcc0591e04586f188095f86d3794a7a196
sgerodes <sgerodes@gmail.com>
2023-03-19 15:26:41 +0100

    add audio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import Fruits from './components/Fruits';
 import Homepage from './components/Homepage';
 import { Routes, Route, Link } from "react-router-dom";
+import Birds from './components/Birds';
 
 
 
@@ -14,12 +15,14 @@ function App() {
         <ul>
           <li><Link to="/" className="nav-item">Homepage</Link></li>
           <li><Link to="/fruits" className="nav-item">Fruits</Link></li>
+          <li><Link to="/birds" className="nav-item">Birds</Link></li>
         </ul>     
 	    </nav>
       
       <Routes> 
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/fruits" element={<Fruits />}></Route>
+        <Route path="/birds" element={<Birds />}></Route>
       </Routes>
 
     </div>
